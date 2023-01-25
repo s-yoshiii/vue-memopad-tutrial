d92da1615546fbab39625d10b7f974152585b749
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-25 12:52:00 +0900

    一覧画面のスタイル調整

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,8 +12,41 @@ const hasMemos = computed(() => {
 <template>
   <div class="home">
     <ul v-if="hasMemos">
-      <li v-for="memo in memos" :key="memo.id">{{ memo.title }}</li>
+      <li v-for="memo in memos" :key="memo.id">
+        <a href="">{{ memo.title }}</a>
+      </li>
     </ul>
     <p v-else>メモがありません。</p>
   </div>
 </template>
+<style scoped>
+ul {
+  margin: 0;
+  padding: 0;
+}
+li {
+  position: relative;
+  list-style: none;
+  border-bottom: 1px solid #ccc;
+  padding-bottom: 10px;
+  padding-left: 2em;
+  margin-bottom: 10px;
+}
+li::before {
+  position: absolute;
+  top: 0.25em;
+  left: 0.5em;
+  content: "";
+  display: inline-block;
+  width: 1em;
+  height: 0.5em;
+  border-left: 2px solid #42b983;
+  border-bottom: 2px solid #42b983;
+  transform: rotate(-45deg);
+}
+li a {
+  color: #42b983;
+  text-decoration: none;
+  display: block;
+}
+</style>
