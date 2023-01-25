c767137ad069f7b016f82ce51b51e541ed29d5a2
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-25 12:38:32 +0900

    メモの一覧を表示する

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
+<script setup>
+import { computed } from "vue";
+import { useStore } from "vuex";
+const store = useStore();
+const memos = computed(() => {
+  return store.state.memos;
+});
+const hasMemos = computed(() => {
+  return store.state.memos.length > 0;
+});
+</script>
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul v-if="hasMemos">
+      <li v-for="memo in memos" :key="memo.id">{{ memo.title }}</li>
+    </ul>
+    <p v-else>メモがありません。</p>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
