a3201b649537e1d1a0fa0e07dd73f087cb486d6b
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-25 12:59:00 +0900

    編集画面のルーティング設定

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import NewView from "../views/NewView.vue";
+import EditView from "../views/EditView.vue";
 
 const routes = [
   {
@@ -9,18 +10,14 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-  {
     path: "/new",
     name: "new",
     component: NewView,
+  },
+  {
+    path: "/edit/:id",
+    name: "edit",
+    component: EditView,
   },
 ];
 
---
+++ src/views/EditView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { useRoute } from "vue-router";
+const route = useRoute();
+</script>
+
+<template>
+  <div>Edit View{{ route.params.id }}</div>
+</template>
+
+<style></style>
